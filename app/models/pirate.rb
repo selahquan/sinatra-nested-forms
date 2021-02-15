b144a7e21b3a1d50e815c8ce76624a6cecd9532a
Josh <josh.handon@gmail.com>
2021-02-15 17:26:10 -0500

    commit tree 2a5d17dc5d5001f58c190cd3a2e83452e5a2bf0b

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+    attr_reader :name, :weight, :height
+
+    @@allpirates = []
+
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        @@allpirates << self
+    end
+
+    def self.all
+        @@allpirates
+    end
+
+    def self.clear
+        @@allpirates = []
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_reader :name, :type, :booty
+
+    @@allships = []
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        @@allships << self
+    end
+
+    def self.all
+        @@allships
+    end
+
+    def self.clear
+        @@allships = []
+    end
 end
